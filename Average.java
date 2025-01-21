564d2b11e0e5f7081a64c83ecfb0c65b4e25744f
ojas kaushik <ojaskaushik99@gmail.com>
2025-01-21 22:44:00 +0530

    Average Of PCM

---
+++ Average.java
@@ -0,0 +1,10 @@
+public class Average{
+   public static void main(String[] args){
+       int M = 94 , P = 95, C = 96;
+       int sum = M+P+C;
+       int average = sum/3;
+
+       System.out.println("Sam's Average Mark in PCM is" + average);
+    
+       }
+}
