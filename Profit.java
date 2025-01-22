14cb7792b55777a74b43e0e0b715272560b9e7ef
ojas kaushik <ojaskaushik99@gmail.com>
2025-01-22 11:20:47 +0530

    Profit Percentage

---
+++ Profit.java
@@ -0,0 +1,9 @@
+public class Profit{
+   public static void main(String[] args){
+      float costPrice = 129; float sellPrice = 191; float profit = 
+      sellPrice - costPrice; float percentage = (profit/costPrice) * 
+      100;
+      System.out.println("The Cost Price is" + costPrice + "And Selling Price is" + sellPrice + "The Profit is" + profit);
+      System.out.println("And The Profit Percentage is" + percentage);
+}
+}
