ffcaa1bfd6a52b10c6c5d3a4376f5ed0293b468c
ojas kaushik <ojaskaushik99@gmail.com>
2025-01-22 11:04:29 +0530

    10.8 Km In Miles

---
+++ Miles.java
@@ -0,0 +1,7 @@
+public class Miles{
+   public static void main(String[] args){
+      double Km = 10.8;
+      double miles = Km/1.6;
+      System.out.println("The Distance" + Km + "In Miles, is" + miles);
+   }
+}
